ad35c1eb83c22b554193bf37e0acfcdcb16ec8f9
ingeniumed <ingeniumed@users.noreply.github.com>
2023-05-03 10:26:10 +1000

    Bump up the use of 7.4 everywhere else

--- vip-block-data-api.php
+++ vip-block-data-api.php
@@ -8,7 +8,7 @@
  * Version: 1.0.0
  * Requires at least: 5.6.0
  * Tested up to: 6.1.0
- * Requires PHP: 7.4
+ * Requires PHP: 8.0
  * License: GPL-3
  * License URI: https://www.gnu.org/licenses/gpl-3.0.html
  *
