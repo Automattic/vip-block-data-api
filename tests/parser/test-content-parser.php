8b06ef16f1d8a038ffb77e5e229c4a17d0edc3c1
Alec Geatches <alec.geatches@automattic.com>
2024-04-08 11:02:36 -0600

    Add test for before_parse filter

--- tests/parser/test-content-parser.php
+++ tests/parser/test-content-parser.php
@@ -13,62 +13,6 @@ namespace WPCOMVIP\BlockDataApi;
 class ContentParserTest extends RegistryTestCase {
 	/* Multiple attributes */
 
-	public function test_block_filter_via_code() {
-		$this->register_block_with_attributes( 'test/block1', [
-			'content' => [
-				'type'     => 'string',
-				'source'   => 'html',
-				'selector' => 'div.a',
-			],
-		] );
-
-		$this->register_block_with_attributes( 'test/block2', [
-			'url' => [
-				'type'      => 'string',
-				'source'    => 'attribute',
-				'selector'  => 'img',
-				'attribute' => 'src',
-			],
-		] );
-
-		$html = '
-			<!-- wp:test/block1 -->
-			<div class="a">Block 1</div>
-			<!-- /wp:test/block1 -->
-
-			<!-- wp:test/block2 -->
-			<img src="/image.jpg" />
-			<!-- /wp:test/block2 -->
-		';
-
-		$expected_blocks = [
-			[
-				'name'       => 'test/block1',
-				'attributes' => [
-					'content' => 'Block 1',
-				],
-			],
-		];
-
-		$block_filter_function = function ( $is_block_included, $block_name ) {
-			if ( 'test/block2' === $block_name ) {
-				return false;
-			} else {
-				return true;
-			}
-		};
-
-		add_filter( 'vip_block_data_api__allow_block', $block_filter_function, 10, 2 );
-		$content_parser = new ContentParser( $this->registry );
-		$blocks         = $content_parser->parse( $html );
-		remove_filter( 'vip_block_data_api__allow_block', $block_filter_function, 10, 2 );
-
-		$this->assertArrayNotHasKey( 'errors', $blocks );
-		$this->assertNotEmpty( $blocks, sprintf( 'Unexpected parser output: %s', wp_json_encode( $blocks ) ) );
-		$this->assertArrayHasKey( 'blocks', $blocks, sprintf( 'Unexpected parser output: %s', wp_json_encode( $blocks ) ) );
-		$this->assertEquals( $expected_blocks, $blocks['blocks'] );
-	}
-
 	public function test_parse_multiple_attributes_from_block() {
 		$this->register_block_with_attributes( 'test/captioned-image', [
 			'caption' => [
