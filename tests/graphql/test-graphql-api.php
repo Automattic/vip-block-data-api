7bfc3c684112c413ee2cbaa2192722bcb0aa9675
Alec Geatches <alec.geatches@automattic.com>
2024-04-24 14:32:20 -0600

    Add test for array values in GraphQL

--- tests/graphql/test-graphql-api.php
+++ tests/graphql/test-graphql-api.php
@@ -27,22 +27,22 @@ class GraphQLAPITest extends RegistryTestCase {
 
 	public function test_get_blocks_data() {
 		$html = '
-            <!-- wp:paragraph -->
-            <p>Welcome to WordPress. This is your first post. Edit or delete it, then start writing!</p>
-            <!-- /wp:paragraph -->
+			<!-- wp:paragraph -->
+			<p>Welcome to WordPress. This is your first post. Edit or delete it, then start writing!</p>
+			<!-- /wp:paragraph -->
 
-            <!-- wp:quote -->
-            <blockquote class="wp-block-quote"><!-- wp:paragraph -->
-            <p>This is a heading inside a quote</p>
-            <!-- /wp:paragraph -->
+			<!-- wp:quote -->
+			<blockquote class="wp-block-quote"><!-- wp:paragraph -->
+			<p>This is a heading inside a quote</p>
+			<!-- /wp:paragraph -->
 
-            <!-- wp:quote -->
-            <blockquote class="wp-block-quote"><!-- wp:heading -->
-            <h2 class="wp-block-heading">This is a heading</h2>
-            <!-- /wp:heading --></blockquote>
-            <!-- /wp:quote --></blockquote>
-            <!-- /wp:quote -->
-        ';
+			<!-- wp:quote -->
+			<blockquote class="wp-block-quote"><!-- wp:heading -->
+			<h2 class="wp-block-heading">This is a heading</h2>
+			<!-- /wp:heading --></blockquote>
+			<!-- /wp:quote --></blockquote>
+			<!-- /wp:quote -->
+		';
 
 		$expected_blocks = [
 			'blocks' => [
@@ -267,5 +267,73 @@ class GraphQLAPITest extends RegistryTestCase {
 		$flattened_blocks = GraphQLApi::flatten_inner_blocks( $inner_blocks, '1' );
 
 		$this->assertEquals( $expected_blocks, $flattened_blocks );
+	}
+
+	public function test_array_data_in_attribute() {
+		$html = '
+			<!-- wp:table -->
+			<figure class="wp-block-table">
+				<table>
+					<thead>
+						<tr>
+							<th>Header A</th>
+							<th>Header B</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>Value A</td>
+							<td>Value B</td>
+						</tr>
+						<tr>
+							<td>Value C</td>
+							<td>Value D</td>
+						</tr>
+					</tbody>
+					<tfoot>
+						<tr>
+							<td>Footer A</td>
+							<td>Footer B</td>
+						</tr>
+					</tfoot>
+				</table>
+			</figure>
+			<!-- /wp:table -->
+		';
+
+		$expected_blocks = [
+			'blocks' => [
+				[
+					'name'       => 'core/table',
+					'attributes' => [
+						[
+							'name'  => 'hasFixedLayout',
+							'value' => false,
+						],
+						[
+							'name'  => 'head',
+							'value' => '[{"cells":[{"content":"Header A","tag":"th"},{"content":"Header B","tag":"th"}]}]',
+						],
+						[
+							'name'  => 'body',
+							'value' => '[{"cells":[{"content":"Value A","tag":"td"},{"content":"Value B","tag":"td"}]},{"cells":[{"content":"Value C","tag":"td"},{"content":"Value D","tag":"td"}]}]',
+						],
+						[
+							'name'  => 'foot',
+							'value' => '[{"cells":[{"content":"Footer A","tag":"td"},{"content":"Footer B","tag":"td"}]}]',
+						],
+					],
+					'id'         => '6',
+				],
+			],
+		];
+
+		$post = $this->factory()->post->create_and_get( [
+			'post_content' => $html,
+		] );
+
+		$blocks_data = GraphQLApi::get_blocks_data( $post );
+
+		$this->assertEquals( $expected_blocks, $blocks_data );
 	}
 }
