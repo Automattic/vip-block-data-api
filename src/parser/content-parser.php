65418c5c3d6998084760830c47a4f04701142261
chriszarate <chris.zarate@automattic.com>
2025-12-15 09:54:55 -0700

    Parser: Allow empty content

--- src/parser/content-parser.php
+++ src/parser/content-parser.php
@@ -146,9 +146,10 @@ class ContentParser {
 
 		$this->warnings = [];
 
-		$has_blocks = has_blocks( $post_content );
+		// An empty `post_content` fails `has_blocks` check, but is still valid.
+		$content_is_empty = empty( trim( $post_content ) );
 
-		if ( ! $has_blocks ) {
+		if ( ! ( $content_is_empty || has_blocks( $post_content ) ) ) {
 			$error_message = join(' ', [
 				sprintf( 'Error parsing post ID %d: This post does not appear to contain block content.', $post_id ),
 				'The VIP Block Data API is designed to parse Gutenberg blocks and can not read classic editor content.',
